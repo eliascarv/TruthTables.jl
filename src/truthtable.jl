a61b3fcf7fa1ea6268f75a54ad7b678a6bb97275
Elias Carvalho <eliascarvdev@gmail.com>
2022-05-09 16:47:50 -0300

    Add docstring template

--- src/truthtable.jl
+++ src/truthtable.jl
@@ -24,14 +24,25 @@ end
 # show mode
 const SHOW_MODE = Ref(:default)
 
-showmode!() = (SHOW_MODE[] = :default)
+"""
+    TruthTables.showmode!(mode::Symbol = :default)
 
+Description...
+
+# Examples
+
+```julia
+# code...
+```
+"""
 function showmode!(mode::Symbol)
     if mode ∉ (:default, :bit, :letter)
         throw(ArgumentError("Invalid show mode, use :default, :bit or :letter."))
     end
     SHOW_MODE[] = mode
 end
+
+showmode!() = (SHOW_MODE[] = :default)
 
 # formatters
 _bit_formatter(v, i, j) = Int(v)
